8bd843419f8b4c3d049e0da3f5c39bc9b2853c4e
Titouan <titouanphilippe57@gmail.com>
2025-10-02 21:33:09 +0200

    Update RButtons.cs

--- Assets/Rhythm/RButtons.cs
+++ Assets/Rhythm/RButtons.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class RButtons : MonoBehaviour
 {
     private InputActions input = null;
-    private float x_click=0;
-    private float y_click=0;
-    private float click_dist=0;
     private float timer = 0f;
+    public float size = 1;
+    public float delay = 1f; //Must be over 0, else it'll always be perfect score
+    //delay for great score, perfect is below 0.5 of it, it's in seconds
 
     private void OnEnable()
     {
@@ -24,9 +25,60 @@ public class RButtons : MonoBehaviour
     private void OnClick(InputAction.CallbackContext value)
     {
         Vector2 v = Mouse.current.position.ReadValue();
-        x_click = v.x;
-        y_click = v.y;
-        click_dist = Mathf.Sqrt((gameObject.transform.position.x - x_click) * (gameObject.transform.position.x - x_click) + (gameObject.transform.position.y - y_click) * (gameObject.transform.position.y - y_click));
+        float x_click = v.x;
+        float y_click = v.y;
+        float click_dist = Mathf.Sqrt((gameObject.transform.position.x - x_click) * (gameObject.transform.position.x - x_click) + (gameObject.transform.position.y - y_click) * (gameObject.transform.position.y - y_click));
+        int score;
+
+        if (click_dist <= size*0.5)
+        {
+            score=0;
+        }
+        else if (click_dist <= size)
+        {
+            score = 1;
+        }
+        else if (click_dist <= size*1.3)
+        {
+            score = 2;
+        }
+        else if (click_dist <= size*1.7)
+        {
+            score = 3;
+        }
+        else if (click_dist <= size*2.5)
+        {
+            score = 4;
+        }
+        else
+        {
+            score = 5;
+        }
+
+        if (timer <= delay*0.5) 
+        {
+            //score+=0;
+        }
+        else if (timer <= delay)
+        {
+            score += 1;
+        }
+        else if (timer <= delay*1.25)
+        {
+            score += 2;
+        }
+        else if (timer <= delay*1.5)
+        {
+            score += 3;
+        }
+        else if (timer <= delay*2)
+        {
+            score += 4;
+        }
+        else
+        {
+            score += 5;
+        }
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
