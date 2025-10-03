0b96ea7593543ec8a2c2895b88bbc18b16b17ae8
RubenStender <rubenstender@gmail.com>
2025-10-03 19:34:23 +0200

    Different accuracy
    Added a Perfect, Good and miss into the Rythm game

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,37 +10,53 @@ public class GameManager : MonoBehaviour
     public int combo;
 
     public TextMeshProUGUI comboText;
-    
+    public TextMeshProUGUI feedbackText;
 
     void Start()
     {
         instance = this;
-
         plushieSlider.value = points;
         combo = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
         comboText.text = $"Combo: {combo}";
     }
 
-    public void NoteHit()
+    public void PerfectHit()
     {
-        Debug.Log("Hit on Time");
-
+        Debug.Log("Perfect Hit!");
         combo++;
         points += 10;
         plushieSlider.value = points;
+        ShowFeedback("Perfect!");
+    }
+
+    public void GoodHit()
+    {
+        Debug.Log("Good Hit!");
+        combo++;
+        points += 5; // less than perfect
+        plushieSlider.value = points;
+        ShowFeedback("Good!");
     }
 
     public void NoteMissed()
     {
         Debug.Log("Missed Note");
-
         points -= 2;
         plushieSlider.value = points;
         combo = 0;
+        ShowFeedback("Miss!");
+    }
+
+    private void ShowFeedback(string message)
+    {
+        if (feedbackText != null)
+        {
+            feedbackText.text = message;
+            // Optionally fade it out later
+        }
     }
 }
--- Assets/NoteObject.cs
+++ Assets/NoteObject.cs
@@ -5,34 +5,51 @@ public class NoteObject : MonoBehaviour
     public bool canBePressed;
     public KeyCode keyToPress;
 
-    private bool scored = false; // Ensures we only score once
+    private bool scored = false;
+    private string currentHitType = ""; // "Perfect" or "Good"
 
     void Update()
     {
         if (!scored && Input.GetKeyDown(keyToPress) && canBePressed)
         {
-            scored = true;               // Mark as scored
-            gameObject.SetActive(false); // Hide the note
-            GameManager.instance.NoteHit();
+            scored = true;
+
+            if (currentHitType == "Perfect")
+            {
+                GameManager.instance.PerfectHit();
+            }
+            else if (currentHitType == "Good")
+            {
+                GameManager.instance.GoodHit();
+            }
+
+            gameObject.SetActive(false); // prevent exit from triggering after hit
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Activator"))
+        if (other.CompareTag("PerfectActivator"))
         {
             canBePressed = true;
+            currentHitType = "Perfect";
+        }
+        else if (other.CompareTag("GoodActivator"))
+        {
+            canBePressed = true;
+            currentHitType = "Good";
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (!scored && other.CompareTag("Activator"))
+        // only miss if the note was never hit
+        if (!scored && (other.CompareTag("PerfectActivator") || other.CompareTag("GoodActivator")))
         {
-            scored = true; // Mark as scored so it doesn't trigger multiple times
             canBePressed = false;
+            scored = true; // prevent multiple calls
             GameManager.instance.NoteMissed();
-            gameObject.SetActive(false); // Hide the note after missing
+            gameObject.SetActive(false);
         }
     }
 }
